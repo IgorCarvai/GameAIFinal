75f1453710ee97279d87eb73c675b92c15288977
IgorCarvai <carvai@rpi.edu>
2017-11-20 15:15:07 -0500

    Added a simple base wiht a health script

---
+++ itPlays/Assets/Scripts/baseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseScript : MonoBehaviour {
+
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+        health = 100;
+	}
+
+    void decreaseHealth()
+    {
+        health = health = -10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
