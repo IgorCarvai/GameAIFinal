97c8488335529f950726d6a518b4bb095b399424
IgorCarvai <carvai@rpi.edu>
2017-11-20 14:39:14 -0500

    added the buttons to add and remove towers

---
+++ itPlays/Assets/Scripts/airTowerBut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class airTowerBut : MonoBehaviour {
+
+    bool activateAir;
+    Text text;
+    string og;
+    // Use this for initialization
+    void Start () {
+        activateAir = false;
+        text = GetComponentInChildren<Text>();
+        og = text.text;
+    }
+
+    public void setAirFalse()
+    {
+        activateAir = false;
+        text.text = og;
+    }
+    public void setAirTrue()
+    {
+        activateAir = true;
+        text.text = "click to set location"; 
+    }
+    public bool GetAir()
+    {
+        return activateAir;
+    }
+}
---
+++ itPlays/Assets/Scripts/groundTowerBut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class groundTowerBut : MonoBehaviour {
+    bool activateGround;
+    Text text;
+    string og;
+    // Use this for initialization
+    void Start()
+    {
+        activateGround = false;
+        text = GetComponentInChildren<Text>();
+        og = text.text;
+    }
+
+    public void setGroundFalse()
+    {
+        activateGround = false;
+        text.text = og;
+    }
+    public void setGroundTrue()
+    {
+
+        activateGround = true;
+        text.text = "click to set location";
+
+    }
+    public bool GetGround()
+    {
+        return activateGround;
+    }
+}
