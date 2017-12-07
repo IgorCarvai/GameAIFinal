e0db30658f255ae4cd6d54d65d4deadbb1b4212d
IgorCarvai <carvai@rpi.edu>
2017-12-06 19:01:38 -0500

    Base scripts now knwos where all teh minons are and towers

--- itPlays/Assets/Scripts/baseScript.cs
+++ itPlays/Assets/Scripts/baseScript.cs
@@ -10,23 +10,115 @@ public class baseScript : MonoBehaviour {
     float lastUpdate;
     public Text hp;
     public Text cointcount;
+    public GameObject GT;
+    public GameObject AT;
+    List<GameObject> groundTowers;
+    List<GameObject> airTowers;
+    List<GameObject> groundMinions;
+    List<GameObject> airMinions;
+
 
     // Use this for initialization
     void Start () {
+        groundTowers = new List<GameObject>();
+        airTowers = new List<GameObject>();
+        groundMinions = new List<GameObject>();
+        airMinions = new List<GameObject>();
         health = 100;
         coins = 100;
-	}
+        findGTsATs();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        findGTsATs();
+        findGMsAMs();
+        UpdateMinions();
+        UpdateGui();
+
+    }
+
+    //since minions can be destroyed, we need to remove the ones that are null
+    void UpdateMinions()
+    {
+        for (int i = 0; i < airMinions.Count;)
+        {
+            if(airMinions[i] == null)
+            {
+                airMinions.RemoveAt(i);
+                i = i - 1;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (int i = 0; i < groundMinions.Count;)
+        {
+            if (airMinions[i] == null)
+            {
+                groundMinions.RemoveAt(i);
+                i = i - 1;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+    }
+
+    //goes through all the objects in the game and finds the Ground and Air towers and appends them to the appropriate lists
+    void findGTsATs()
+    {
+        foreach(GameObject tower in GameObject.FindGameObjectsWithTag("GT"))
+        {
+            if (!groundTowers.Contains(tower))
+            {
+                groundTowers.Add(tower);
+            }
+
+        }
+        foreach (GameObject tower in GameObject.FindGameObjectsWithTag("AT"))
+        {
+            if (!airTowers.Contains(tower))
+            {
+                airTowers.Add(tower);
+            }
+        }
+    }
+
+
+    //goes through all the objects in the game and finds the Ground and Air minions and appends them to the appropriate lists
+    void findGMsAMs()
+    {
+        foreach (GameObject minion in GameObject.FindGameObjectsWithTag("GM"))
+        {
+            if (!groundMinions.Contains(minion))
+            {
+                groundMinions.Add(minion);
+            }
+
+        }
+        foreach (GameObject minion in GameObject.FindGameObjectsWithTag("AM"))
+        {
+            if (!airMinions.Contains(minion))
+            {
+                airMinions.Add(minion);
+            }
+        }
+    }
+
 
     public void decreaseHealth()
     {
         health = health-10;
     }
 	
-	// Update is called once per frame
-	void Update () {
-
-        //coins go up 10 at a time
-        if (Time.time - lastUpdate >= 1f )
+    void UpdateGui()
+    {    //coins go up 10 at a time
+        if (Time.time - lastUpdate >= 1f)
         {
             lastUpdate = Time.time;
             coins = coins + 10;
@@ -34,12 +126,13 @@ public class baseScript : MonoBehaviour {
 
         hp.text = "HP = " + health;
         cointcount.text = "Gold = " + coins;
-
     }
+  
     public void minionKilled()
     {
         coins = coins + 1;
     }
+
     public void spawnTower()
     {
         coins = coins - 100;
