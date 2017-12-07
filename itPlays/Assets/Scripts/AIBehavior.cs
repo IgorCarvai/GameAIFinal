6f50eb8189a9a81c4ce3816ceed6cd3a2679fcad
AnIllegalPanda <danhendricks36@gmail.com>
2017-12-06 22:19:34 -0500

    added Minion Movement

---
+++ itPlays/Assets/Scripts/AIBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBehavior : MonoBehaviour {
+    public List<char[]> map = new List<char[]>();
+    public int money;
+    public int cost;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+    
+
+
+
+
+
+
+//TO DO:
+/*AI needs to Keep track of the board and whats on it
+ * AI needs to keep track of how much money it has, and compare it to board statistics
+ */
